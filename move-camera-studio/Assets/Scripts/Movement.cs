5630c02a668cc432eae295c4fe6c421cd181a619
fanaimi <emiliano.fantasia@gmail.com>
2021-10-07 22:47:52 +0100

    completing demo

--- move-camera-studio/Assets/Scripts/Movement.cs
+++ move-camera-studio/Assets/Scripts/Movement.cs
@@ -12,7 +12,7 @@ public class Movement : MonoBehaviour
     
     // REFERENCES
     [SerializeField] private CharacterController controller;
-    
+    [SerializeField] private Transform cam;  
     
     
     
@@ -32,21 +32,36 @@ public class Movement : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
 
-        if (direction.magnitude >= 0.1)
+        if (direction.magnitude >= 0.1 )
         {
             // THIS WILL ROTATE THE PLAYER IN THE DIRECTION HE'S GOING TO
             // ATAN2 returns an angle in radians so we convert it into degrees 
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            // float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            // connecting to camera 
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            
             // smoothing rotation
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            
             // applying rotation
             // transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
             
             // now with smoothed angle
             transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
             
+            // we multiply to Vector3.forward to transform a rotation into a direction
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            
             // {* Time.deltaTime} makes it frame rate independent
-            controller.Move(direction * speed * Time.deltaTime);
+            // controller.Move(direction * speed * Time.deltaTime);
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            
+            
+            
+            // apply MAIN CAMERA to CAM for PLAYER
+            
+            
         }
     }
 }
